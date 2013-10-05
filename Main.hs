5b4a46e6f3f1c520fa1e880dc5dfbb2b4610de5e
Greg Kettler <gkettler@alum.mit.edu>
2013-10-04 22:38:22 -0500

    break things up a little

--- Main.hs
+++ Main.hs
@@ -38,6 +38,8 @@ needleAction = do
     text $ T.pack $ show $ align  string1  string2
 
 
+format l = [map fst l, map snd l]
+
 align :: BS.ByteString -> BS.ByteString -> [BS.ByteString]
 align da db = map BS.pack $ format $ reverse $ traceback lena lenb
     where
@@ -45,17 +47,22 @@ align da db = map BS.pack $ format $ reverse $ traceback lena lenb
         lenb = BS.length db
         a = BS.append space da
         b = BS.append space db
+
+        memscore :: Array (Int, Int) Int
         memscore = listArray ((0,0), (lena, lenb))
-                [score x y | x <- [0..lena], y <- [0..lenb]]
+                             [score x y | x <- [0..lena], y <- [0..lenb]]
         infix 5 @@
         (@@) i j = memscore !(i,j)
+
+        score :: Int -> Int -> Int
         score 0 _ = 0
         score _ 0 = 0
         score x y = maximum [(x-1 @@ y - 1)  + difference x y,
-                      x-1  @@ y,
-                      x    @@ y-1]
+                             x-1  @@ y,
+                             x    @@ y-1]
                         where difference x y | BS.index a x == BS.index b y = 1 --matches
                                              | otherwise                    = 0 --doesn't match
+
         traceback :: Int -> Int -> [(Char, Char)]
         traceback 0 0 = []
         traceback x y     | x == 0               = (' '    , b @! y):traceback 0     (y-1)
@@ -63,4 +70,3 @@ align da db = map BS.pack $ format $ reverse $ traceback lena lenb
                           | x @@ y == x @@ y-1   = (' ', b @! y)    :traceback x     (y-1)
                           | x @@ y == x-1 @@ y   = (a @! x, ' ')    :traceback (x-1) y
                           | otherwise            = (a @! x, b @! y) :traceback (x-1) (y-1)
-        format l = [map fst l, map snd l]
