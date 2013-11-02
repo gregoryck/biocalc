7f4e017057f822dd7119f44398d09f2157ca13b8
Greg Kettler <gkettler@alum.mit.edu>
2013-11-01 22:20:45 -0500

    scratch pad expts

---
+++ ParTest.hs
@@ -0,0 +1,51 @@
+import Control.Monad.Par
+import Data.IntMap as IM
+
+import Data.Traversable
+import Data.Array hiding ((!))
+
+
+pr :: Key -> Int -> IO ()
+pr a = print
+
+
+theMap = fromList [(1, 100), (2, 200), (3, 300)]
+
+printAtKey :: Key -> Int -> IO ()
+printAtKey k times = print $ take times $ repeat $ theMap ! k
+
+printAtKey' :: Key -> Int -> IO String
+printAtKey' k times = case IM.lookup k theMap of Nothing  -> do
+                                                    print "key not found"
+                                                    return "key not found"
+                                                 Just val -> do
+                                                    print $ take times $ repeat val
+                                                    return "printed"
+
+-- traverseWithKey ::
+
+doIt = traverseWithKey printAtKey theMap
+doIt' = traverseWithKey printAtKey' theMap
+
+
+-- what of list monad
+listFromKey k times = take k $ repeat times
+
+
+
+doList = traverseWithKey listFromKey theMap
+
+
+
+--rp = get $ runPar $ spawn $ return 1
+
+
+x :: Par (IVar Int)
+x = spawn $ return 1
+
+
+x' = do
+    xx <- spawn $ return (1 :: Int)
+    y <- get xx
+    return y
+    --return $ get xx
