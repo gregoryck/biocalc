a83f54534aaac52b6cb21908507f322523ff07e0
Greg Kettler <gkettler@alum.mit.edu>
2014-01-30 08:27:30 -0600

    compiles without warnings. But the alignment is off. Why?

--- AlignPar.hs
+++ AlignPar.hs
@@ -2,10 +2,8 @@
 module AlignPar where
 import Data.ByteString.Char8 as BS hiding (map, concat, zip)
 
-import Control.Monad.Par.Scheds.Trace as P
+--import Control.Monad.Par.Scheds.Trace as P
 import Data.Array
---import Data.IntMap hiding (map)
-import Data.Traversable
 --import Debug.Trace (trace)
 
 tobs :: String -> BS.ByteString
@@ -31,16 +29,13 @@ data SeqPosition = Top | Side
 align :: Seq -> Seq -> Aln
 align s1 s2 = Aln s1_gaps s2_gaps
     where
-        s1_gaps = addGaps Top grid s1
-        s2_gaps = addGaps Side grid s2
+        s1_gaps = addGaps Side grid s1
+        s2_gaps = addGaps Top grid s2
         grid = myGrid s1 s2
 
 addGaps :: SeqPosition -> Grid -> Seq -> Seq
 addGaps pos g s = BS.pack $ concat $ [letterWithGaps pos x | x <- zip (path g) (BS.unpack s)]
 
-
--- FIXME this behaves differently for the top and side sequences.
--- which are we?
 letterWithGaps :: SeqPosition -> (Cell, Char) -> [Char]
 letterWithGaps Top (Across _, c) = [' ', c]
 letterWithGaps Top (Up _, c) = [c]
@@ -53,19 +48,20 @@ data Cell = Up Int | Across Int | Diag Int | Start Int deriving (Show, Eq, Ord)
 
 data Grid = Grid (Array Int (Array Int Cell)) deriving Show
 
-
 aArray :: Array Int Cell
 aArray = listArray (0,4) [Start 1, Up 1, Up 1, Up 1, Up 1]
 badGrid :: Grid
 badGrid = Grid $ (listArray (0,2) $ repeat aArray)
 
+goodGrid :: Grid
 goodGrid = Grid $ listArray (0,3) $ [listArray (0,3) [Start 0, Up 1, Up 2, Up 3],
                                      listArray (0,3) [Across 1, Across 2, Up 3, Up 4],
                                      listArray (0,3) [Across 2, Diag 2, Diag 3, Diag 4],
                                      listArray (0,3) [Across 3, Diag 2, Diag 3, Diag 4]
                                     ]
 
-gridBounds g@(Grid array0) = (snd $ bounds $ array0,
+gridBounds :: Grid -> (Int, Int)
+gridBounds (Grid array0) = (snd $ bounds $ array0,
                               snd $ bounds $ (array0 ! 0))
 
 
@@ -76,11 +72,11 @@ path g = path' g x y []
 
 -- this will runtime exception on a malformed Grid
 path' :: Grid -> Int -> Int -> [Cell] -> [Cell]
-path' g' x y accum = case cell of
-                        Up score     -> path' g' x (y-1) (cell:accum)
-                        Across score -> path' g' (x-1) y (cell:accum)
-                        Diag score   -> path' g' (x-1) (y-1) (cell:accum)
-                        Start score  -> cell:accum
+path' g' x y accumList = case cell of
+                        Up _     -> path' g' x (y-1) (cell:accumList)
+                        Across _ -> path' g' (x-1) y (cell:accumList)
+                        Diag _   -> path' g' (x-1) (y-1) (cell:accumList)
+                        Start _  -> cell:accumList
                      where
                         cell = lookUp g' x y
 
@@ -130,11 +126,14 @@ similarityScore c1 c2 | c1 == c2  = 2
 (-*) (Across n) n' = Across (n - n')
 (-*) (Up n) n' = Up (n - n')
 (-*) (Diag n) n' = Diag (n - n')
+(-*) (Start n) n' = Start (n - n')
 
 (+*) :: Cell -> Int -> Cell
 (+*) (Diag n) n' = Diag (n + n')
 (+*) (Across n) n' = Across (n + n')
 (+*) (Up n) n' = Up (n + n')
+(+*) (Start n) n' = Start (n + n')
 
+gapPenalty :: Int
 gapPenalty = 3
 
