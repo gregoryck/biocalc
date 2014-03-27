089be041bf70580b7d6478f2036d832fde9af831
Greg Kettler <gkettler@alum.mit.edu>
2014-03-26 22:09:25 -0500

    handle empty seqs

--- testbiocalc.hs
+++ testbiocalc.hs
@@ -3,40 +3,48 @@
 
 import Test.QuickCheck
 import AlignPar as AP
-import Align as A
+--import Align as A
 import Data.Array
 --import Data.Char
 
 import Data.ByteString.Char8 as BS hiding (map, concat, zip)
 
-
 instance Arbitrary BS.ByteString where
     arbitrary :: Gen Seq
     arbitrary = fmap AP.tobs arbitrary
 
-
 equalsItself :: Seq -> Bool
 equalsItself = (\s -> s == s)
-
 
 equalThemselves :: [Seq] -> Bool
 equalThemselves xs = and $ map equalsItself xs
 
-
+emptyOrFirstIsStart :: Grid -> Bool
 emptyOrFirstIsStart g = or [isEmpty g, isStart $ lookUp g 0 0]
 
+isEmpty :: Grid -> Bool
 isEmpty (Grid array0) = or [(bounds array0) == (0,-1),
                             (bounds (array0 ! 0)) == (0,-1)]
 
+isStart :: Cell -> Bool
 isStart (Start _) = True
 isStart _ = False
 
 startIsStart :: Seq -> Seq -> Bool
 startIsStart s1 s2 = emptyOrFirstIsStart $ grid s1 s2
 
+intslessThan10 :: Gen Int
+intslessThan10 = suchThat arbitrary (<10)
+
+lessThan10 :: Int -> Bool
+lessThan10 = (<10)
+
+triviallyTrue :: Property
+triviallyTrue = forAll intslessThan10 lessThan10
 
 main :: IO ()
 main = do
     quickCheck equalsItself
     quickCheck equalThemselves
     quickCheck startIsStart
+    quickCheck triviallyTrue
